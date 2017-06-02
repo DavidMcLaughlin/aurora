d06dabb3bfb940975e4ddc4bef0f7dc80fa2aac5
David McLaughlin <david@dmclaughlin.com>
2017-06-01 22:52:12 -0700

    Real tests

--- src/main/js/components/__tests__/HomePage-test.js
+++ src/main/js/components/__tests__/HomePage-test.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 
-import HomePage from '../HomePage';
+import HomePage, { RoleList } from '../HomePage';
 import Loading from '../Loading';
 
-const mockApi = jest.fn()
-mockApi.getRoleSummary = (callback) => []
+function createMockApi(roles = null) {
+  const api = jest.fn();
+  if (roles != null) {
+    api.getRoleSummary = (callback) => callback({
+      result: {
+        roleSummaryResult: {
+          summaries: roles
+        }
+      }
+    });
+  } else {
+    api.getRoleSummary = (callback) => { };
+  }
+  return api;
+}
+
+const roles = [{role: 'test', jobCount: 0, cronJobCount: 5}];
 
 describe('HomePage suite', () => {
   it('should render a loading div', () => {
-    expect(shallow(<HomePage api={mockApi} />).contains(<Loading />)).toBe(true);
+    const home = shallow(<HomePage api={createMockApi()} />);
+    expect(home.contains(<Loading />)).toBe(true);
   });
-});
+
+  it('should show a role list', () => {
+    const home = shallow(<HomePage api={createMockApi(roles)} />);
+    expect(home.contains(<Loading />)).toBe(false);
+    expect(home.contains(<RoleList roles={roles}/>)).toBe(true);
+  })
+})
